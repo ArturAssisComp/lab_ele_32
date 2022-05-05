a082804a2473cae392c57156d8053b2df7dc0bcd
Artur Assis Alves <60040236+ArturAssisComp@users.noreply.github.com>
2022-05-04 23:39:02 -0300

    Implement cyclic coder/decoder

--- lab2/cyclic_coder.py
+++ lab2/cyclic_coder.py
@@ -18,14 +18,8 @@ class CyclicBitBlockCoder():
     
     Transmission rate -> k / N
 
-    Ex.: To create Hamming code coder, use the following matrix:
-    | 1  0  0  0  1  1  1 | b1
-    | 0  1  0  0  1  0  1 | b2
-    | 0  0  1  0  1  1  0 | b3
-    | 0  0  0  1  0  1  1 | b4
-     b1 b2 b3 b4 p1 p2 p3
     '''
-    def __init__(self, g, N):
+    def __init__(self, g, N, num_of_bits_to_correct=1):
         '''
         Inputs: 
             g -> The generator polynomial must be a list with the coefficients represented
@@ -46,25 +40,21 @@ class CyclicBitBlockCoder():
         self.generator_polynomial = P(g)
         self.codeword_length = N 
         self.information_word_length = N - len(g) + 1
-        self._init_min_weight_error_for_given_syndrome()
+        self._init_min_weight_error_for_given_syndrome(num_of_bits_to_correct)
 
 
-    def _init_min_weight_error_for_given_syndrome(self):
-        pass
-        '''
-        _, syndrome_length = self.parity_check_matrix.shape
-        self.min_weight_error_for_given_syndrome = _generate_dict_with_default_syndromes(syndrome_length, default_value=(None,self.codeword_length+1))
+    def _init_min_weight_error_for_given_syndrome(self, num_of_bits_to_correct):
+        syndrome_length = self.codeword_length - self.information_word_length
 
-        #Possible errors:
-        current_error = [0] * self.codeword_length
-        for _ in range(2 ** self.codeword_length):
-            syndrome = tuple((np.array(current_error) @ self.parity_check_matrix) % 2)
-            weight = sum(current_error)
-            if weight <= self.min_weight_error_for_given_syndrome[syndrome][1]: # The choose of the comparison operator makes difference here. <= instead of <?
-                #Update the error for the current syndrome:
-                self.min_weight_error_for_given_syndrome[syndrome] = (tuple(current_error), weight)
-            _binary_increment(current_error)
-        '''
+        #Correct last bit:
+        last_bit_error_coef = tuple([0] * (self.codeword_length - 1) + [1])
+        last_bit_error_polynomial = P(last_bit_error_coef)
+        syndrome_coef = tuple((last_bit_error_polynomial % self.generator_polynomial).coef % 2)
+        self.min_weight_error_for_given_syndrome = dict()
+        self.min_weight_error_for_given_syndrome[syndrome_coef] = (last_bit_error_coef, 1) 
+
+        #Implement correction for more bits:
+        # ...
         
 
     def encode(self, information_words_array):
@@ -83,52 +73,32 @@ class CyclicBitBlockCoder():
 
 
     def decode(self, codewords_array):
-        '''
         codeword_length = self.codeword_length
         word_length = self.information_word_length
-        default_error = [0] * codeword_length
         if not isinstance(codewords_array, type(np.array([], dtype=np.ubyte))): raise TypeError("The input must be a numpy array.")
         if  codewords_array.dtype != np.ubyte: raise TypeError("The input must be a numpy array with element of type np.ubyte")
         if len(codewords_array) % codeword_length != 0: raise ValueError(f"The input must have a len that is multiple of {codeword_length}")
         
-        # Matriz de verificacao de paridade
-        H_T = self.parity_check_matrix
 
         candidate_information_words = []
         for ini in range(0, len(codewords_array), codeword_length):
             end = ini + codeword_length
-            received_codeword = codewords_array[ini : end]
-            syndrome = received_codeword @ H_T
-            syndrome %= 2
-            syndrome = tuple(syndrome)
-            candidate_for_error, _ = self.min_weight_error_for_given_syndrome[syndrome] 
-            if candidate_for_error is None: canditate_for_error = default_error
-            candidate_codeword = np.bitwise_xor(candidate_for_error, received_codeword)
-            candidate_information_words.extend(candidate_codeword[0 : word_length])
+            received_codeword = list(codewords_array[ini : end])
+            received_codeword_polynomial = P(received_codeword)
+            #Meggit decoding algorithm:
+            for _ in range(self.codeword_length):
+                syndrome = tuple((received_codeword_polynomial % self.generator_polynomial).coef % 2)
+                if self.min_weight_error_for_given_syndrome.get(syndrome)  is not None:
+                    received_codeword[-1] ^= 1 #fix the last bit
+
+                #rotate the codeword:
+                received_codeword = received_codeword[-1:] + received_codeword[:-1]
+                received_codeword_polynomial = P(received_codeword)
+            candidate_information_word = (received_codeword_polynomial // self.generator_polynomial).coef % 2
+            candidate_information_word = list(candidate_information_word) + [0]*(word_length - len(candidate_information_word))
+            candidate_information_words.extend(candidate_information_word)
         return np.array(candidate_information_words, dtype=np.ubyte)
-        '''
-        pass
 
-
-def _generate_dict_with_default_syndromes(syndrome_length, default_value=None):
-    """
-    Description: Creates a dictionary with 2**syndrome_length pairs of key:value. 
-    The keys are tuples that represent binary values from (0, 0, .., 0) to 
-    (1, 1, ..., 1) with length equals to 'syndrome_length'. All of those pairs 
-    have value equals to 'default_value'.
-    T = O(2**syndrome_length)
-    """
-    '''
-    current_syndrome = [0] * syndrome_length
-    result_dict = dict()
-
-    for _ in range(2**syndrome_length):
-        result_dict[tuple(current_syndrome)] = default_value
-        _binary_increment(current_syndrome)
-
-    return result_dict
-    '''
-    pass
 
 
 
@@ -171,28 +141,6 @@ class TestCBBC__I__(unittest.TestCase):
 
     def test_CBBC__I__003(self):
         pass
-        '''
-        hamming_encode_matrix = np.array([[1, 0, 0, 0, 1, 1, 1],  # b1
-                                          [0, 1, 0, 0, 1, 0, 1],  # b2
-                                          [0, 0, 1, 0, 1, 1, 0],  # b3
-                                          [0, 0, 0, 1, 0, 1, 1]]) # b4
-                                         # b1 b2 b3 b4 p1 p2 p3
-
-        hamming_coder = GenericParityBitBlockCoder(hamming_encode_matrix)
-        self.assertEqual(hamming_coder.information_word_length, 4)
-        self.assertEqual(hamming_coder.codeword_length, 7)
-        self.assertEqual(hamming_coder.encode_matrix.tolist(), [[1, 0, 0, 0, 1, 1, 1],  
-                                                               [0, 1, 0, 0, 1, 0, 1],  
-                                                               [0, 0, 1, 0, 1, 1, 0], 
-                                                               [0, 0, 0, 1, 0, 1, 1]])
-        self.assertEqual(hamming_coder.parity_check_matrix.tolist(), [[1, 1, 1], 
-                                                                      [1, 0, 1], 
-                                                                      [1, 1, 0], 
-                                                                      [0, 1, 1], 
-                                                                      [1, 0, 0], 
-                                                                      [0, 1, 0], 
-                                                                      [0, 0, 1]])
-        '''
 
 
 
